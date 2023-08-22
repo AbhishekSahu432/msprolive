1b5fdbcf4cbcca4c3f89ae1dc4c2f3694754fa0c
AbhishekSahu432 <142717398+AbhishekSahu432@users.noreply.github.com>
2023-08-22 13:40:48 +0530

    Add files via upload

---
+++ RoApi/RApi/home/StuSerializer.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Stu
+class StuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Stu
+        fields='__all__'
---
+++ RoApi/RApi/home/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Stu
+admin.site.register(Stu)
+# Register your models here.
+
